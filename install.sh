d2e39d94915b8eace6f4ca1815aa3b31a314b1a1
George Beckstein <george.beckstein@gmail.com>
2018-09-24 14:42:42 -0400

    Added git credential caching

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ cp ./intel-hex-mode/intel-hex-mode.el ~/.emacs.d/modes/
 echo "(add-to-list 'custom-theme-load-path \"~/.emacs.d/themes\")" >> ~/.emacs
 echo "(load-theme 'pomegranate-explosion t)" >> ~/.emacs
 echo "(load-file \"~/.emacs.d/modes/intel-hex-mode.el\")" >> ~/.emacs
+
+apt-get install git
+git config credential.helper store
+git config --global credential.helper "cache --timeout 43200"
