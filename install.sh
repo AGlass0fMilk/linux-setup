6b4ae46153dfea67de59dfaaa08e110d52944581
George Beckstein <george.beckstein@gmail.com>
2018-09-24 14:37:04 -0400

    Updated script

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ mkdir ~/.emacs.d/modes
 cp ./pomegranate-explosion-theme.el ~/.emacs.d/themes/
 cp ./intel-hex-mode/intel-hex-mode.el ~/.emacs.d/modes/
 
-echo "(add-to-list\'custom-theme-load-path \"~/.emacs.d/themes\")" >> ~/.emacs
-echo "(load-theme \'pomegranate-explosion)" >> ~/.emacs
+echo "(add-to-list 'custom-theme-load-path \"~/.emacs.d/themes\")" >> ~/.emacs
+echo "(load-theme 'pomegranate-explosion t)" >> ~/.emacs
 echo "(load-file \"~/.emacs.d/modes/intel-hex-mode.el\")" >> ~/.emacs
